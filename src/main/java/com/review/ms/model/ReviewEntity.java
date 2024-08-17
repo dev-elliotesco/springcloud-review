88ab0b3ee3f8e876faa5ae202f2cc5f984f00f3f
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-17 11:34:20 -0500

    add model layer

---
+++ src/main/java/com/review/ms/model/ReviewEntity.java
@@ -0,0 +1,22 @@
+package com.review.ms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "reviews")
+public class ReviewEntity {
+    @Id
+    private String id;
+    private String productId;
+    private String userId;
+    private String review;
+    private int rating;
+}
